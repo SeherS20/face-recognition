ba06198191eee98f5e7a4e12c56d39b56ddbe419
SeherS20 <99300690+SeherS20@users.noreply.github.com>
2023-04-23 16:52:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90,
+    force_flash: false
+});
+camera = document.getElementById("camera")
+
+Webcam.attach('#camera');
+function snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML= '<img id="captured_image" src = "'+ data_uri +'" >'
+    })
+}
+console.log ('ml5 version', ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oB1Ym-9Gi/model.json',modelLoaded) 
+function modelLoaded(){
+    console.log('model loaded!')
+}
