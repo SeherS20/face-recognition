bb7167eab7d01e4e76f4d2c7d1f63037434ca8d8
SeherS20 <99300690+SeherS20@users.noreply.github.com>
2023-04-29 14:24:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,23 @@ function snapshot(){
     })
 }
 console.log ('ml5 version', ml5.version);
-classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oB1Ym-9Gi/model.json',modelLoaded) 
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oB1Ym-9Gi/model.json', modelLoaded) 
 function modelLoaded(){
     console.log('model loaded!')
+}
+
+function check() {
+    img = document.getElementById("captured_image")
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error)
+    }
+    else {
+        console.log (results)
+        document.getElementById("result_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = (results[0].confidence.toFixed(2))*100 + "%"
+    }
 }
